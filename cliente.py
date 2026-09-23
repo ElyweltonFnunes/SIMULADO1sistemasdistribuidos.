c3812d57163cad1734537b2417bb416ad8190844
ElyweltonFnunes <elywelton22@gmail.com>
2026-09-23 20:31:56 -0300

    Implement TCP client to send and receive data

--- cliente.py
+++ cliente.py
@@ -1,1 +1,9 @@
+cat > cliente.py <<'EOF'
+import socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
+ cliente.connect(("127.0.0.1", 5005))
+ cliente.sendall("PEDIDO-42".encode("utf-8"))
+ resposta = cliente.recv(1024).decode("utf-8")
+ print("Resposta recebida:", resposta)
+EOF
 
